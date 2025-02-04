913fd71f65b028dc22e585633658f3092bf951dc
Sebastian Giraldo <sebasvelez293@hotmail.com>
2025-02-04 13:46:35 -0500

    se configuran los endpoints para obtener noticias tendecia y buscar articulos mediante palabras clave

--- src/store/API/NewsApi.js
+++ src/store/API/NewsApi.js
@@ -15,13 +15,28 @@ export const NewsApi = createApi({
 	}),
 
 	endpoints: (builder) => ({
-		searchNews: builder.query({
+		searchArticle: builder.query({
 			query: ({query, language}) => ({
 				url: '/v2/search/articles',
 				params: {query, language},
 			}),
+			transformResponse: (response) => {
+				const { data } = response;
+				return data;
+			},
+		}),
+		
+		getTrending: builder.query({
+			query: ({topic, language}) => ({
+				url: '/v2/trendings',
+				params: {topic, language},
+			}),
+			transformResponse: (response) => {
+				const { data } = response;
+				return data;
+			},
 		}),
 	}),
 });
 
-export const {useSearchNewsQuery} = NewsApi;
+export const {useSearchArticleQuery, useGetTrendingQuery} = NewsApi;
