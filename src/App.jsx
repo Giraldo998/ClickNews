91b700f0ddfc932e59011f5382bbd04fbff82dbf
Sebastian Giraldo <sebasvelez293@hotmail.com>
2025-02-04 22:26:20 -0500

    se crea un identificador para el componente NewsGrid para establecer disntos estilos en la disposicion y organizacion de articulos se mueven los esilos de los articulos a NewsGrid para mejorar la especificidad

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ export const App = () => {
 			{/* banner */}
 			{/* </header> */}
 			<main style={{marginTop: '80px'}}>
-				<NewsGrid topic="politics" />
-				<NewsGrid topic="General" />
-				<NewsGrid topic="technology" />
-				<NewsGrid topic="economy" />
+				<NewsGrid topic="economy" id='1' />
+				<NewsGrid topic="General" id='2' />
+				<NewsGrid topic="technology" id='3' />
+				<NewsGrid topic="culture" id='4' />
 			</main>
 			{}
 		</>
