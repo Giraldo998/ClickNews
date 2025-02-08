bfa3eaccb7dbcf82249686b3c2b55ccdc7d9497e
Sebastian Giraldo <sebasvelez293@hotmail.com>
2025-02-08 17:14:57 -0500

    feat: se agrega el componente DefaultNews para renderizar newsGrid y manejar las llamadas a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import { Navbar, NewsGrid } from "./components";
 import { SearchedNews } from "./pages/SearchedNews";
+import { DefaultNews } from "./pages/DefaultNews";
 
 export const App = () => {
 	const [keyWord, setKeyWord] = useState('');
 
-	const searchNotice = (NewKKeyWord) => {
-		
-		setKeyWord(NewKKeyWord);
-	}
+	const searchNotice = (NewKKeyWord) => setKeyWord(NewKKeyWord);
 	
 	return (
 		<>
 			<Navbar keyWordNotice={(event) => searchNotice(event)}/>
 				
 			<main style={{marginTop: '80px'}}>
-				<SearchedNews keyWord={keyWord}/>
+				{/* <SearchedNews keyWord={keyWord}/> */}
+				<DefaultNews />
 			</main>
 			{}
 		</>
---
+++ src/pages/DefaultNews.jsx
@@ -0,0 +1,25 @@
+import {useEffect} from 'react';
+import {useGetTrendingQuery} from '../store/API/NewsApi';
+import {NewsGrid} from '../components';
+
+export const DefaultNews = () => {
+   
+	const {data, isLoading, error} = useGetTrendingQuery({topic: 'economy', language: 'es'});
+
+	useEffect(() => {
+		if (error) console.error('Error:', error);
+	}, [error]);
+
+   if (isLoading) console.log('Loading...');
+
+   if (data) {
+      return (
+         <>
+            <NewsGrid topic={data} id="1" />
+            <NewsGrid topic={data} id='2'/>
+            <NewsGrid topic={data} id='3'/>
+         </>
+      );
+   }
+   return null;
+};
