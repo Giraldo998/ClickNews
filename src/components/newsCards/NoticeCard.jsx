0047775ea471d22545be97a05e0a607320e89329
Sebastian Giraldo <sebasvelez293@hotmail.com>
2025-02-28 02:25:21 -0500

    refactor: mejorar la semántica y escalabilidad de NoticeCard.jsx
    - Reestructurar la composición del componente NoticeCard.jsx para mayor claridad y mantenimiento
    - Agregar lógica para mostrar el autor del artículo
    - Actualizar estilos en noticecard.css para reflejar los cambios
    - Actualizar estilos en newsGrid.css para mejorar la estructura de los
      contenedores de articulos

--- src/components/newsCards/NoticeCard.jsx
+++ src/components/newsCards/NoticeCard.jsx
@@ -6,18 +6,25 @@ export const NoticeCard = ({
 	excerpt: description,
 	thumbnail: image,
 	url,
+	authors,
 }) => {
 
 	return (
-		<article className={`card_container card_${index}`}>
+		<article className={`card_container card_${index} card`}>
 			<a href={url} target='_blank' className="card_content">
 				<header className='card_header'>
+					<div className="image_container">
+						<img src={image} alt="" className="card_image" />
+					</div>
 					<h3 className='card_title'>{title}</h3>
-					<p className='card_description'>{description}</p>
 				</header>
-				<div className="image_container">
-					<img src={image} alt="" className="card_image" />
-				</div>
+				<div className="description_container">
+					<p className='card_description'>{description}</p>
+					{(authors!='')
+						? <p>Autor: <span>{authors}</span></p>
+						: <p>Autor: <span>Anónimo</span></p>
+					}
+					</div>
 			</a>
 		</article>
 	);
